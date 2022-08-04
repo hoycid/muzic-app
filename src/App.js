9950220fd9cfb748a80f2950892bc4a70ff9886b
hoycid <quilangcidrex@gmail.com>
2022-08-04 15:58:31 +0800

    create banner component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar/Navbar";
+import Banner from "./components/Banner/Banner";
 
 import "./App.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Banner />
     </div>
   );
 }
