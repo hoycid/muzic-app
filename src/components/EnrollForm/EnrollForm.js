b1659af0ac749cba0cc086ed2c9b5186e3077fd8
hoycid <quilangcidrex@gmail.com>
2022-08-04 16:35:58 +0800

    change instrument input to select

--- src/components/EnrollForm/EnrollForm.js
+++ src/components/EnrollForm/EnrollForm.js
@@ -6,11 +6,14 @@ const EnrollForm = () => {
       <form>
         <h3>SCHEDULE A SESSION</h3>
         <input type="text" placeholder="Student Name" />
-        <input type="number" placeholder="Student Age" />
+        <input type="number" placeholder="Student Age" min="1" max="80" />
         <input type="email" placeholder="Email" />
         <input type="text" placeholder="Phone" />
-        <input placeholder="Location" />
-        <input placeholder="Instrument" />
+        <input placeholder="Address" />
+        <select>
+          <option>Guitar</option>
+          <option>Piano</option>
+        </select>
         <button type="submit">Submit</button>
       </form>
     </div>
