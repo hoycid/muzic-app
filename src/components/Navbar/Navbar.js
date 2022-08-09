13e7501a1a36cc5fa0281d188717f753eebd38bc
hoycid <quilangcidrex@gmail.com>
2022-08-09 15:47:15 +0800

    Added placeholder logo to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import classes from "./Navbar.module.css";
 
+import logo from "../../assets/images/logo.png";
+
 const Navbar = () => {
   return (
     <header className={classes["navigation-bar"]}>
-      <img src="" alt="muzic-logo"></img>
+      <img src={logo} alt="muzic-logo"></img>
       <ul>
         <li>Music lessons</li>
         <li>Workshops</li>
